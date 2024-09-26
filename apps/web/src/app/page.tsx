86e4a39d41df203e6116fa494cf2a89d84af3789
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2024-09-26 14:24:06 +0530

    validate script

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,10 @@
-import { add } from "@new/sample-lib";
+import { add } from '@new/sample-lib'
 export default function Home() {
+  // const aa = (a: number, b: number) => a + b
   return (
     <div>
       <h1>Welcome to web!</h1>
       <p>{add(1, 25)}</p>
     </div>
-  );
+  )
 }
