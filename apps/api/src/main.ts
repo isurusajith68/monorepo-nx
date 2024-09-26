c8d948ce0474c9a31be9f31d58311e556e68165e
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2024-09-26 13:57:09 +0530

    add nx

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { add } from '@new/sample-lib';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(add(1, 5));
   await app.listen(3000);
 }
 bootstrap();
