56d4fc138c2908f67cb09b15ab7baab238fba2ef
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2024-09-27 20:59:54 +0530

    Refactor App.js to update rendered content

--- apps/react-app/src/App.tsx
+++ apps/react-app/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">jsoij</header>
+      <header className="App-header">jsoijd</header>
     </div>
   )
 }
