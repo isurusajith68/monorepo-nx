adfd9bd76443a870def7620e72a78ca4c60681f2
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2024-09-27 19:38:53 +0530

    dajd

--- apps/react-app/src/App.js
+++ apps/react-app/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  return <div className="App">test git work flow 1</div>
+  return <div className="App">test git work flow 11</div>
 }
 
 export default App
